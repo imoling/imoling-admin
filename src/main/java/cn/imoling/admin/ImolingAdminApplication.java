319441a5b1b2093999f2bfd0fc9feed2bdae4e20
imoling <laopo520>
2019-03-12 15:16:26 +0800

    首次提交

---
+++ src/main/java/cn/imoling/admin/ImolingAdminApplication.java
@@ -0,0 +1,13 @@
+package cn.imoling.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImolingAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImolingAdminApplication.class, args);
+    }
+
+}
